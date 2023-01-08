26ce6d6de03d48d008ae63844c381e77d5f76bd7
2re1million <56553678+2re1million@users.noreply.github.com>
2023-01-08 02:26:19 +0100

    Update exponential_growth.py
    Nytt regnestykke. OK - har sjekka

--- exponential_growth.py
+++ exponential_growth.py
@@ -1,16 +1,12 @@
 import pandas as pd
 import streamlit as st
 
-
 # Set title and intro message
 st.title('Exponential Savings 🤑')
 
-
 st.info("💡 Exponential growth is called the eight wonder of the world. It is a rapid increase in a quantity over time, characterized by a rate that is proportional to the current value of the quantity.")
 
-
 st.write('Try it out you self... Calculate how much your savings will grow each year!')
-
 
 # Get user input
 initial_savings = st.number_input(
@@ -42,28 +38,17 @@ annual_inflation_rate = st.slider(
     step=0.1
 )
 
-# Calculate the annual income and interest rate
+# Calculate the annual income
 annual_income = monthly_savings * 12
-interest_rate = (annual_interest_rate / 100) + 1
 
 # Create a DataFrame with the number of years
 df = pd.DataFrame({'year': range(1, years+1)})
 
-# Calculate the savings for each year
-df['savings'] = annual_income * interest_rate ** df['year']
-
 # Calculate the total savings for each year
-df['total_savings'] = df['savings'].cumsum() + initial_savings
-
-# Calculate the monthly interest on the initial savings
-monthly_interest = initial_savings * (interest_rate - 1) / 12
-
-# Add the monthly interest to the total savings
-df['total_savings'] += monthly_interest
+df['total_savings'] = initial_savings * (1 + annual_interest_rate/100) ** df['year'] + annual_income * (((1 + annual_interest_rate/100) ** df['year']) - 1) / (annual_interest_rate/100)
 
 # Adjust the total savings for inflation
-inflation_rate = (annual_inflation_rate / 100) + 1
-df['total_savings'] = df['total_savings'] / inflation_rate ** df['year']
+df['total_savings'] = df['total_savings'] / (1 + annual_inflation_rate/100) ** df['year']
 
 # Calculate the total savings for the last year
 last_year_savings = df['total_savings'].iloc[-1]
@@ -71,8 +56,9 @@ last_year_savings = df['total_savings'].iloc[-1]
 # Write the total savings for the last year in the "metrics" element
 st.write(f'Total savings after {years} years: ', int(last_year_savings), ',-')
 
-# Show the result in a line graph:
+# Show the result in a line chart:
 st.line_chart(df, x="year", y="total_savings")
 
 # Show the results in the Streamlit app
 st.dataframe(df)
+
